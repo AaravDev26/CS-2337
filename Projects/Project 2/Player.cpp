e9ef501df7225b659f624f0c1c06c2feabc66970
AaravDev26 <119716812+AaravDev26@users.noreply.github.com>
2022-12-02 21:41:15 -0600

    Create Player.cpp

---
+++ Projects/Project 2/Player.cpp
@@ -0,0 +1,15 @@
+// Aarav Dev
+// axd220001
+
+#include "Player.h" // includes player class
+
+// default constructor, sets all integer variables to zero
+Player::Player()
+{
+    hits = 0;
+    outs = 0;
+    strikeouts = 0;
+    walks = 0;
+    hitsByPitch = 0;
+    sacrifices = 0;
+}
